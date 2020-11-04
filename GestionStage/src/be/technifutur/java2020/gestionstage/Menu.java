fef176c24364c2ff3d19864c3baa42766cb6057a
Jonathan <arabia.jonathan@hotmail.be>
2020-11-04 10:38:55 +0100

    creation menu

--- GestionStage/src/be/technifutur/java2020/gestionstage/Menu.java
+++ GestionStage/src/be/technifutur/java2020/gestionstage/Menu.java
@@ -1,12 +1,27 @@
 package be.technifutur.java2020.gestionstage;
 
+import java.util.Scanner;
+
 public class Menu {
-    public void displayMenu(){
+    Scanner scanner = new Scanner(System.in);
+    String input = null;
+
+    public void displayMenu() {
         showMenu();
+        input = scanner.nextLine();
+        while (!(input.equalsIgnoreCase("q"))) {
+            int choice = (Integer.parseInt(input));
+            switch (choice){
+                case 1 :
+                    break;
+            }
+        }
     }
 
     private void showMenu() {
-        System.out.println(""+
-                "1. Créer un stage");
+        System.out.println("" +
+                "Veuillez choisir une option.\n" +
+                "1. Créer un stage.\n" +
+                "q. Quitter l'application.");
     }
 }
